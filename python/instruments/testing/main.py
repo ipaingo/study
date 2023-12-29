0e2959a51698f35fb2243f40e11a18db83488e53
ipaingo <szimenkova7@gmail.com>
2023-12-29 10:38:10 +0300

    casetools 7 done

--- python/instruments/testing/main.py
+++ python/instruments/testing/main.py
@@ -3,9 +3,9 @@ import sys
 import statistics
 from split_lib import split_data
 
+
 # запись данных из файла.
 def read_data_from_file(filename):
-
     # это вернем.
     rows_csv = []
     with open(filename) as csvfile:
@@ -18,14 +18,15 @@ def read_data_from_file(filename):
     rows_csv.pop(0)
     return rows_csv
 
+
 # статистика и вывод в формате 'начало,конец,количество,среднее,медиана'.
 def calculate_statistics(data_stat):
-
     # отметка начала отрывка. поместим в нее первое значение.
 
     start = float(data_stat[0].split()[0])
 
-    # массив значений в пятиминутном отрывке, который надо будет обработать статистике. началом является первая отметка, поэтому поместим в массив соответствующее ей значение.
+    # массив значений в пятиминутном отрывке, который надо будет обработать статистике.
+    # началом является первая отметка, поэтому поместим в массив соответствующее ей значение.
     data = [int(data_stat[0].split()[1])]
 
     # этот массив мы вернем для вывода.
@@ -36,33 +37,36 @@ def calculate_statistics(data_stat):
         # проверяем, является ли следующий элемент разделителем.
         if float(data_stat[i].split()[0]) == -1:
 
-            # если так, запишем длинную строку из нужных нам значений, разделенных запятой. мода не работает в линуксе, поэтому пришлось ее убрать.
-            result.append(",".join([str(start), data_stat[i-1].split()[0], str(len(data)), str(statistics.mean(data)), str(statistics.median(data))]))
+            # если так, запишем длинную строку из нужных нам значений, разделенных запятой.
+            # мода не работает в линуксе, поэтому пришлось ее убрать.
+            result.append(",".join([str(start), data_stat[i - 1].split()[0], str(len(data)), str(statistics.mean(data)),
+                                    str(statistics.median(data))]))
 
             # обновим массив данных для статистики.
             data = []
 
-        elif data == []:
+        elif not data:
 
             # костыль. обновим начало.
             start = float(data_stat[i].split()[0])
             data.append(int(data_stat[i].split()[1]))
 
-        else:    
+        else:
             # если мы не вышли за пять минут, прибавим к массиву для статистики новый элемент.
             data.append(int(data_stat[i].split()[1]))
 
     return result
 
+
 if __name__ == '__main__':
-    if (len(sys.argv) != 3):
+    if len(sys.argv) != 3:
         print("Неправильное количество аргументов")
     else:
         raw_data = read_data_from_file(sys.argv[1])
 
         # в конец добавляем -1, потому что это число будет разделителем между промежутками в N секунд.
         raw_data.append("-1")
-    
+
         # вывод.
         divided_data = split_data(raw_data, int(sys.argv[2]))
 
--- python/instruments/testing/main_test.py
+++ python/instruments/testing/main_test.py
@@ -7,57 +7,91 @@
 # поэтому pytest отработает с 5 failed, 3 passed.
 # (пробовала все испортить - тогда 8 failed.)
 
+import statistics
 import pytest
-from main import * # сами функции, собственно.
+from main import *  # сами функции, собственно.
 from split_lib import split_data
 
 
-def test_read_data_from_file(): # 1. проверка на отсутствие файла.
-	try:
-		read_data_from_file('exampl.csv')
-	except FileNotFoundError:
-		pytest.fail('Файл не найден.')
+def test_read_data_from_file():  # 1. проверка на отсутствие файла.
+    try:
+        read_data_from_file('exampl.csv')
+    except FileNotFoundError:
+        pytest.fail('Файл не найден.')
 
-def test_roots(): # 2. проверка на отсутствие доступа к файлу.
-	try:
-		read_data_from_file('err.csv')
-	except PermissionError: # проверяется с помощью zimenkov/inf/2sem/lab5/err.csv (к нему закрыт доступ).
-		pytest.fail('Файл недоступен для чтения.')
 
-def test_csv(): # 3. ex1.csv там же, где err.csv. просто испорченный example.csv.
-	x = read_data_from_file('ex1.csv')
-	for i in range(len(x)):
-		if len(x[i].split()) != 2:
-			pytest.fail('Формат файла не удовлетворяет шаблону csv.')
+def test_roots():  # 2. проверка на отсутствие доступа к файлу.
+    try:
+        read_data_from_file('err.csv')
+    except PermissionError:  # проверяется с помощью zimenkov/inf/2sem/lab5/err.csv (к нему закрыт доступ).
+        pytest.fail('Файл недоступен для чтения.')
 
-def test_data(): # 4. проверка на количество колонок. выявляет ошибку, если колонка только одна.
-	try:
-		calculate_statistics(['1', '2 354', '-1', '5 54'])
-	except IndexError:
-		pytest.fail('В файле присутствует строка с одной колонкой.')
 
-def test_type(): # 5. проверка на то, что данные являются числами.
-	x = read_data_from_file('ex1.csv')
-	try:
-		calculate_statistics(x)
-	except ValueError:
-		pytest.fail('Неверный тип данных.')
+def test_csv():  # 3. ex1.csv там же, где err.csv. просто испорченный example.csv.
+    x = read_data_from_file('ex1.csv')
+    for i in range(len(x)):
+        if len(x[i].split()) != 2:
+            pytest.fail('Формат файла не удовлетворяет шаблону csv.')
 
-def test_time(): # 6. проверка на верное деление на интервалы.
-	if split_data(['1 200', '3 2345', '5 200', '7 100'], 5) != ['1 200', '3 2345', '5 200', '-1', '7 100']:
-		pytest.fail('Неверные временные интервалы.') # маленький массив можно заменить на пример верно разделенного example.csv.
-		
-def test_amount(): # 7. проверка на количество интервалов.
-	count = 0
-	x = split_data(['1 200','3 2345','5 200','7 100'], 5)
-	for i in range(len(x)): # маленькие массивы, опять же, можно заменить. это просто пример для демонстрации работы теста.
-		if x[i] == '-1':
-			count += 1
-	if count != 1:
-		pytest.fail('Неверное количество интервалов.')
 
-def test_stat(): # 8. проверка на верный подсчет статистики.
-	x = read_data_from_file('ex2.csv')
-	x.append('-1')
-	if calculate_statistics(split_data(x, 5)) != ['1.0,5,3,915,200', '7.0,7,1,100,100']:
-		pytest.fail('Статистика подсчитана неверно.') # справа просто результат работы программы на ex2.csv.
+def test_data():  # 4. проверка на количество колонок. выявляет ошибку, если колонка только одна.
+    try:
+        calculate_statistics(['1', '2 354', '-1', '5 54'])
+    except IndexError:
+        pytest.fail('В файле присутствует строка с одной колонкой.')
+
+
+def test_type():  # 5. проверка на то, что данные являются числами.
+    x = read_data_from_file('ex1.csv')
+    try:
+        calculate_statistics(x)
+    except ValueError:
+        pytest.fail('Неверный тип данных.')
+
+
+def test_time():  # 6. проверка на верное деление на интервалы.
+    if split_data(['1 200', '3 2345', '5 200', '7 100'], 5) != ['1 200', '3 2345', '5 200', '-1', '7 100']:
+        pytest.fail('Неверные временные интервалы.')
+        # маленький массив можно заменить на пример верно разделенного example.csv.
+
+
+def test_amount():  # 7. проверка на количество интервалов.
+    count = 0
+    x = split_data(['1 200', '3 2345', '5 200', '7 100'], 5)
+    for i in range(
+            len(x)):  # маленькие массивы, опять же, можно заменить. это просто пример для демонстрации работы теста.
+        if x[i] == '-1':
+            count += 1
+    if count != 1:
+        pytest.fail('Неверное количество интервалов.')
+
+
+def test_stat():  # 8. проверка на верный подсчет статистики.
+    x = read_data_from_file('ex2.csv')
+    x.append('-1')
+    if calculate_statistics(split_data(x, 5)) != ['1.0,5,3,915,200', '7.0,7,1,100,100']:
+        pytest.fail('Статистика подсчитана неверно.')  # справа просто результат работы программы на ex2.csv.
+
+
+def test_max():  # 9. проверка на... "maxint"?... слишком большое число, короче.
+    x = read_data_from_file('example.csv')
+    try:
+        calculate_statistics(split_data(x, 999999))
+    except statistics.StatisticsError:
+        pytest.fail('Временной промежуток должен быть меньше общей длины эксперимента!')
+
+
+def test_min():  # 10. проверка на отрицательное число.
+    x = read_data_from_file('ex2.csv')
+    try:
+        calculate_statistics(split_data(x, -999999))
+    except statistics.StatisticsError:
+        pytest.fail('Временной промежуток должен быть положительным!')
+
+
+def test_zero():  # 11. проверка на ноль.
+    x = read_data_from_file('ex2.csv')
+    try:
+        calculate_statistics(split_data(x, 0))
+    except statistics.StatisticsError:
+        pytest.fail('Временной промежуток должен быть положительным!')
