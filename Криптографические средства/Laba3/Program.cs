9d6048fa9b4db608de57b1b4e5ffa1bd97dbe2e5
ipaingo <szimenkova7@gmail.com>
2024-04-02 22:14:59 +0300

    cripto lab 1 in progress

---
+++ Криптографические средства/Laba3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Numerics;
+using System;
+using System.Text;
+using System.Security.Cryptography;
+
+var mainDirPath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar;
+var dirAlicePath = mainDirPath + "Alice" + Path.DirectorySeparatorChar;
+var dirBobPath = mainDirPath + "Bob" + Path.DirectorySeparatorChar;
+var fileAlicePublicKeyPath          = dirAlicePath + "PublicRSA.key";
+var fileAlicePrivateKeyPath         = dirAlicePath + "PrivateRSA.key";
+var fileBobPublicKeyPath            = dirBobPath + "PublicRSA.key";
+var fileBobPrivateKeyPath           = dirBobPath + "PrivateRSA.key";
+var fileAliceOriginalMessagePath    = dirAlicePath + "message.txt";
+var fileAliceMessageCertPath        = dirAlicePath + "message.cert";
+var fileAliceMessageKeyPath        = dirAlicePath + "message.key";
+var fileAliceEncodedMessagePath     = dirAlicePath + "encoded";
+var fileBobDecodedMessagePath       = dirBobPath + "decoded.txt";
+
+void WriteBytes(string filePath, byte[] bytes) {
+    using BinaryWriter writer = new(File.Open(filePath, FileMode.OpenOrCreate));
+    writer.Write(bytes);
+}
+
+void WriteString(string filePath, string str) {
+    using StreamWriter writer = new(File.Open(filePath, FileMode.OpenOrCreate));
+    writer.Write(str);
+}
+
+void GenerateAndWriteRSAKeys(string publicKeyPath, string privateKeyPath) {
+    RSA rsa = RSA.Create();
+
+    WriteBytes(publicKeyPath, rsa.ExportRSAPublicKey());
+    WriteBytes(privateKeyPath, rsa.ExportRSAPrivateKey());
+}
+
+RSA ReadRSA(string publicKeyPath, string privateKeyPath) {
+    RSA rsa = RSA.Create();
+    rsa.ImportRSAPublicKey(File.ReadAllBytes(publicKeyPath), out int k1);
+    rsa.ImportRSAPrivateKey(File.ReadAllBytes(privateKeyPath), out int k2);
+
+    return rsa;
+}
+
+Aes GenerateAes() {
+    Aes aes = Aes.Create();
+    aes.GenerateKey();
+    return aes;
+}
+
+Aes ReadAes(string keyPath) {
+    Aes aes = Aes.Create();
+    aes.Key = File.ReadAllBytes(keyPath);
+
+    return aes;
+}
+
+void EncodeMessage(string message, Aes aes, RSA aliceRSA, RSA bobRSA, out byte[] encodedKey, out byte[] encodedMessage, out byte[] cert) {
+    encodedMessage = aes.EncryptEcb(new UTF8Encoding().GetBytes(message), PaddingMode.ANSIX923);
+    encodedKey = bobRSA.Encrypt(aes.Key, RSAEncryptionPadding.OaepSHA256);
+    cert = aliceRSA.SignData(new UTF8Encoding().GetBytes(message), HashAlgorithmName.SHA256, RSASignaturePadding.Pss);
+}
+
+void DecodeMessage(RSA aliceRSA, RSA bobRSA, byte[] encodedMessage, byte[] encodedKey, out byte[] originalMessage) {
+    Aes aes = Aes.Create();
+    aes.Key = bobRSA.Decrypt(encodedKey, RSAEncryptionPadding.OaepSHA256);
+    originalMessage = aes.DecryptEcb(encodedMessage, PaddingMode.ANSIX923);
+}
+
+bool VerifyMessage(RSA aliceRSA, byte[] message, byte[] cert) {
+    return aliceRSA.VerifyData(message, cert, HashAlgorithmName.SHA256, RSASignaturePadding.Pss);
+}
+
+
+
+
+
+{
+    if (!File.Exists(fileAlicePublicKeyPath) || !File.Exists(fileAlicePrivateKeyPath)) {
+        GenerateAndWriteRSAKeys(fileAlicePublicKeyPath, fileAlicePrivateKeyPath);
+    }
+
+    if (!File.Exists(fileBobPublicKeyPath) || !File.Exists(fileBobPrivateKeyPath)) {
+        GenerateAndWriteRSAKeys(fileBobPublicKeyPath, fileBobPrivateKeyPath);
+    }
+
+    var aliceRSA = ReadRSA(fileAlicePublicKeyPath, fileAlicePrivateKeyPath);
+    var bobRSA = ReadRSA(fileBobPublicKeyPath, fileBobPrivateKeyPath);
+
+    var aliceMessage = File.ReadAllText(fileAliceOriginalMessagePath);
+
+    // Шифрование и подпись
+    {
+        Aes aes = GenerateAes();
+        EncodeMessage(aliceMessage, aes, aliceRSA, bobRSA, out byte[] encodedKey, out byte[] encodedMessage, out byte[] cert);
+
+        WriteBytes(fileAliceEncodedMessagePath, encodedMessage);
+        WriteBytes(fileAliceMessageKeyPath, encodedKey);
+        WriteBytes(fileAliceMessageCertPath, cert);
+    }
+
+    // Дешифрование и проверка подписи
+    {
+        byte[] encodedMessage = File.ReadAllBytes(fileAliceEncodedMessagePath);
+        byte[] encodedKey = File.ReadAllBytes(fileAliceMessageKeyPath);
+        byte[] cert = File.ReadAllBytes(fileAliceMessageCertPath);
+        byte[] originalMessage;
+
+        DecodeMessage(aliceRSA, bobRSA, encodedMessage, encodedKey, out originalMessage);
+        WriteString(fileBobDecodedMessagePath, new UTF8Encoding().GetString(originalMessage));
+
+        bool dataVerified = VerifyMessage(aliceRSA, originalMessage, cert);
+
+        Console.WriteLine(dataVerified);
+    }
+}
--- ксзи/criptography/Program.cs
+++ ксзи/criptography/Program.cs
@@ -1,202 +1,240 @@
-﻿//using System.Numerics;
-//using System;
-//using System.Security.Cryptography;
-//using System.Runtime.CompilerServices;
+﻿using System.Numerics;
+using System;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
 
-//BigInteger BinPow(BigInteger n, BigInteger k, BigInteger mod) {
-//    if (k == 0)
-//        return new BigInteger(1);
-//    if (k == 1)
-//        return n % mod;
-    
-//    var mid = BinPow(n, k / 2, mod);
-//    return mid * mid * (k % 2 == 1 ? n : 1) % mod;
-//}
+// возведение в степень. рекурсия.
+BigInteger BinPow(BigInteger n, BigInteger k, BigInteger mod)
+{
+    if (k == 0)
+        return new BigInteger(1);
+    if (k == 1)
+        return n % mod;
 
-//List<int> generateLowPrimes() {
-//    bool[] sieve = new bool[2000];
-//    for (int i = 2; i < 2000; i++) {
-//        if (!sieve[i]) {
-//            for (int j = i * i; j < 2000; j += i) {
-//                sieve[j] = true;
-//            }
-//        }
-//    }
+    // деление с остатком.
+    var mid = BinPow(n, k / 2, mod);
+    return mid * mid * (k % 2 == 1 ? n : 1) % mod;
+}
 
-//    var primes = new List<int>();
-//    for (int i = 2; i < 2000; i++) {
-//        if (!sieve[i]) {
-//            primes.Add(i);
-//        }
-//    }   
+// функция поиска простых чисел с помощью решета Эратосфена.
+// используется для предподсчета массива небольших простых чисел.
+List<int> generateLowPrimes()
+{
+    bool[] sieve = new bool[2000];
+    for (int i = 2; i < 2000; i++)
+        if (!sieve[i])
+            for (int j = i * i; j < 2000; j += i)
+                sieve[j] = true;
 
-//    return primes;
-//}
+    var primes = new List<int>();
+    for (int i = 2; i < 2000; i++)
+        if (!sieve[i])
+            primes.Add(i);
 
-//var lowPrimes = generateLowPrimes();
+    return primes;
+}
 
-//bool isMayBePrime(BigInteger prime) {
-//    foreach (var lp in lowPrimes) {
-//        if (lp == prime)
-//            return true;
-//        if (prime % lp == 0) {
-//            return false;
-//        }
-//    }
+var lowPrimes = generateLowPrimes();
 
-//    int s = 0;
-//    BigInteger t = prime - 1;
-//    while (t % 2 == 0) {
-//        t /= 2;
-//        s++;
-//    }
+// определяет, является ли число простым. тест Миллера-Рабина.
+bool isMaybePrime(BigInteger prime)
+{
+    for (int i = 0; i < lowPrimes.Count; i++)
+    {
+        int lp = lowPrimes[i];
+        if (lp == prime)
+            return true;
+        if (prime % lp == 0)
+        {
+            return false;
+        }
+    }
 
-//    int iters = 256;
-//    while (iters-- > 0) {
-//        var a = RandBigIntegerInRange(2, prime - 2);
-//        var x = BinPow(a, t, prime);
-//        if (x == 1 || x == prime - 1)
-//            continue;
+    int s = 0;
+    BigInteger t = prime - 1;
+    while (t % 2 == 0)
+    {
+        t /= 2;
+        s++;
+    }
 
-//        bool flag = true;
-//        for (int i = 1; i < s; i++) {
-//            x = BinPow(x, 2, prime);
-//            if (x == 1)
-//                return false;
-//            if (x == prime - 1) {
-//                flag = false;
-//                break;
-//            }
-//        }
-//        if (flag) {
-//            return false;
-//        }
-//    }
+    int iters = 256;
+    while (iters-- > 0)
+    {
+        var a = RandBigIntegerInRange(2, prime - 2);
+        var x = BinPow(a, t, prime);
+        if (x == 1 || x == prime - 1)
+            continue;
 
-//    return true;
-//}
+        bool flag = true;
+        for (int i = 1; i < s; i++)
+        {
+            x = BinPow(x, 2, prime);
+            if (x == 1)
+                return false;
+            if (x == prime - 1)
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+        {
+            return false;
+        }
+    }
 
-//BigInteger getRandomPrime() {
-//    while (true) {
-//        int bits = 256;
-//        BigInteger min = 1, max = 2; 
-//        while (--bits > 0) {
-//            min *= 2;
-//            max *= 2;
-//        }
-//        min++;
-//        max--;
-//        var prime = RandBigIntegerInRange(min, max);
-//        if (prime % 2 == 0)
-//            prime += 1;
-//        if (!isMayBePrime(prime))
-//            continue;
-//        return prime;
-//    }
-//}
+    return true;
+}
+
+// генерирует случайное число в заданном отрезке.
+BigInteger RandBigIntegerInRange(BigInteger minValue, BigInteger maxValue)
+{
+    if (minValue == maxValue) return minValue;
+
+    var random = new Random();
+    BigInteger zeroBasedUpperBound = maxValue - 1 - minValue;
+    byte[] bytes = zeroBasedUpperBound.ToByteArray();
+
+    byte lastByteMask = 0b11111111;
+    for (byte mask = 0b10000000; mask > 0; mask >>= 1, lastByteMask >>= 1)
+    {
+        if ((bytes[bytes.Length - 1] & mask) == mask) break;
+    }
+
+    while (true)
+    {
+        random.NextBytes(bytes);
+        bytes[bytes.Length - 1] &= lastByteMask;
+        var result = new BigInteger(bytes);
+        if (result <= zeroBasedUpperBound) return result + minValue;
+    }
+}
+
+// функция генерации случайного простого числа.
+BigInteger getRandomPrime()
+{
+    while (true)
+    {
+        // по сути генерирует псевдослучайное число и проверяет,
+        // не получилось ли оно составным.
+        int bits = 256;
+        BigInteger min = 1, max = 2;
+        while (--bits > 0)
+        {
+            min *= 2;
+            max *= 2;
+        }
+        min++;
+        max--;
+        var prime = RandBigIntegerInRange(min, max);
+        if (prime % 2 == 0)
+            prime += 1;
+        if (!isMaybePrime(prime))
+            continue;
+        return prime;
+    }
+}
+
+// функция нахождения наибольшего общего делителя.
+// используется в китайской теореме об остатках.
+BigInteger GCD(BigInteger a, BigInteger b)
+{
+    if (b > a)
+        (a, b) = (b, a);
+    while (b > 0)
+    {
+        a %= b;
+        (a, b) = (b, a);
+    }
+
+    return a;
+}
+
+// функция вычисления закрытого ключа. расширенный алгоритм Евклида.
+BigInteger GCDex(BigInteger a, BigInteger b, out BigInteger x, out BigInteger y)
+{
+    if (a == 0)
+    {
+        x = 0; y = 1;
+        return b;
+    }
+    BigInteger x1, y1;
+    BigInteger d = GCDex(b % a, a, out x1, out y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return d;
+}
+
+// функция генерации RSA ключей.
+void generateRSAKeys(BigInteger e, out BigInteger d, out BigInteger n, out BigInteger p, out BigInteger q)
+{
+    BigInteger f;
+    do
+    {
+        // генерирует случайные простые p и q.
+        p = getRandomPrime();
+        q = getRandomPrime();
+        n = p * q;
+        f = (p - 1) * (q - 1);
+    } while (GCD(e, f) != 1);
+
+    // закрытый ключ d генерируется с помощью расширенного алгоритма Евклида.
+    GCDex(e, f, out d, out BigInteger y);
+    d = (d % f + f) % f;
+}
 
 
-//BigInteger GCD(BigInteger a, BigInteger b) {
-//    if (b > a)
-//        (a, b) = (b, a);
-//    while (b > 0) {
-//        a %= b;
-//        (a, b) = (b, a); 
-//    }
+// получает электронную подпись.
+BigInteger getCert(in BigInteger m, in BigInteger d, in BigInteger n, in BigInteger p, in BigInteger q)
+{
+    // для вычисления используется китайская теорема об остатках.
+    var r1 = BinPow(m % p, d % (p - 1), p);
+    var r2 = BinPow(m % q, d % (q - 1), q);
 
-//    return a;
-//}
+    BigInteger m1, m2;
+    GCDex(q, p, out m1, out BigInteger y1);
+    GCDex(p, q, out m2, out BigInteger y2);
+    m1 = (m1 % p + p) % p;
+    m2 = (m2 % q + q) % q;
 
-//BigInteger GCDex(BigInteger a, BigInteger b, out BigInteger x, out BigInteger y) {
-//	if (a == 0) {
-//		x = 0; y = 1;
-//		return b;
-//	}
-//	BigInteger x1, y1;
-//	BigInteger d = GCDex(b % a, a, out x1, out y1);
-//	x = y1 - (b / a) * x1;
-//	y = x1;
-//	return d;
-//}
+    return (r1 * m1 * q + r2 * m2 * p) % n;
+}
 
-//void generateRSAKeys(BigInteger e, out BigInteger d, out BigInteger n, out BigInteger p, out BigInteger q) {
-//    BigInteger f;
-//    do {
-//        p = getRandomPrime();
-//        q = getRandomPrime();
-//        n = p * q;
-//        f = (p - 1) * (q - 1);
-//    } while (GCD(e, f) != 1);
-//    GCDex(e, f, out d, out BigInteger y);
-//    d = (d % f + f) % f;
-//}
-
-//BigInteger getCert(in BigInteger m, in BigInteger d, in BigInteger n) {
-//    return BinPow(m, d, n);
-//}
-
-//BigInteger getCertFast(in BigInteger m, in BigInteger d, in BigInteger n, in BigInteger p, in BigInteger q) {
-//    // Китайская теорема о остатках: https://studfile.net/preview/6268704/page:14/
-//    var r1 = BinPow(m % p, d % (p - 1), p);
-//    var r2 = BinPow(m % q, d % (q - 1), q);
-
-//    BigInteger m1, m2;
-//    GCDex(q, p, out m1, out BigInteger y1);
-//    GCDex(p, q, out m2, out BigInteger y2);
-//    m1 = (m1 % p + p) % p;
-//    m2 = (m2 % q + q) % q;
-
-//    return (r1 * m1 * q + r2 * m2 * p) % n;
-//}
-
-//bool validateCert(in BigInteger m, in BigInteger cert, in BigInteger e, BigInteger n) {
-//    return m == BinPow(cert, e, n);
-//}
+// проверка электронной подписи.
+bool validateCert(in BigInteger m, in BigInteger cert, in BigInteger e, BigInteger n)
+{
+    return m == BinPow(cert, e, n);
+}
 
 
-//BigInteger m = 1111;
-//BigInteger e = 1337;
-//BigInteger n, d, p, q;
-//generateRSAKeys(e, out d, out n, out p, out q);
+BigInteger m = 1111;
+BigInteger e = 1399;
+BigInteger n, d, p, q;
+generateRSAKeys(e, out d, out n, out p, out q);
 
-//Console.WriteLine($"({n}, {d})");
-//Console.WriteLine($"({n}, {e})");
+Console.WriteLine($"Открытый ключ (n, e): ({n}, {e})");
+Console.WriteLine($"Закрытый ключ (n, d): ({n}, {d})");
 
-//var cert = getCert(m, d, n);
-//var cert2 = getCertFast(m, d, n, p, q);
+var cert = getCert(m, d, n, p, q);
 
-//Console.WriteLine(cert);
-//Console.WriteLine(cert2);
+Console.WriteLine("Электронная подпись:");
+Console.WriteLine(cert);
 
-//if (validateCert(m, cert, e, n)) {
-//    Console.WriteLine("Valid");
-//} else {
-//    Console.WriteLine("Invalid");
-//}
+if (validateCert(m, cert, e, n))
+{
+    Console.WriteLine("Электронная подпись действительна.");
+}
+else
+{
+    Console.WriteLine("Электронная подпись недействительна.");
+}
 
-//if (validateCert(m, cert + 4, e, n)) {
-//    Console.WriteLine("Valid");
-//} else {
-//    Console.WriteLine("Invalid");
-//}
-
-//BigInteger RandBigIntegerInRange(BigInteger minValue, BigInteger maxValue) {
-//    if (minValue == maxValue) return minValue;
-//    var random = new Random();
-//    BigInteger zeroBasedUpperBound = maxValue - 1 - minValue; // Inclusive
-//    byte[] bytes = zeroBasedUpperBound.ToByteArray();
-
-//    byte lastByteMask = 0b11111111;
-//    for (byte mask = 0b10000000; mask > 0; mask >>= 1, lastByteMask >>= 1)
-//    {
-//        if ((bytes[bytes.Length - 1] & mask) == mask) break; // We found it
-//    }
-
-//    while (true)
-//    {
-//        random.NextBytes(bytes);
-//        bytes[bytes.Length - 1] &= lastByteMask;
-//        var result = new BigInteger(bytes);
-//        if (result <= zeroBasedUpperBound) return result + minValue;
-//    }
-//}
+if (validateCert(m, cert + 4, e, n))
+{
+    Console.WriteLine("Измененная электронная подпись действительна.");
+}
+else
+{
+    Console.WriteLine("Измененная электронная подпись недействительна.");
+}
