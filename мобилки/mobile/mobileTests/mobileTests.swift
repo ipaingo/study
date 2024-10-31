8308026fd45f33e2d86d908d67bec5c776e638f9
Sofia <szimenkova7@gmail.com>
2024-10-31 11:32:17 +0300

    created mobile project on swift

---
+++ мобилки/mobile/mobileTests/mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  mobileTests.swift
+//  mobileTests
+//
+//  Created by Sofia on 29.10.2024.
+//
+
+import Testing
+@testable import mobile
+
+struct mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
