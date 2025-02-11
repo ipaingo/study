fbd4595b97032fc10ac3d6eb04e8da664a58416d
ipaingo <szimenkova7@gmail.com>
2025-02-11 20:10:55 +0300

    removed files from prev sem

--- перенести/data analysis/lab5/5.py
+++
@@ -1,9 +0,0 @@
-from sympy import *
-from IPython.display import display
-init_printing()
-
-
-x = Symbol("x")
-expr = (2*x**2 + x - 345)/(x**2 - x + 345)
-print(limit(expr, x, oo))
-
--- перенести/data analysis/lab9/1.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def get_headers():
-    response = requests.get('https://httpbin.org/get') 
-    return response.json()['headers']['Host']
